7cf699065179b17456b1850fbac017cf805291d7
Mayur <ultramayur123@gmail.com>
2022-01-18 23:11:35 +0530

    sm7125-common: Remove some gapps in runtime
    Removed unwanted gapps
    Reference: https://github.com/DerpFest-Devices/device_xiaomi_davinci/blob/12/remove_packages/Android.mk
    
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- common.mk
+++ common.mk
@@ -292,6 +292,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     RealmeParts
 
+# Remove Packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
