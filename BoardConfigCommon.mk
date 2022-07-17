06d4f067456bc1a43087792bcd7b9fcd93e3387d
Alexander Koskovich <akoskovich@pm.me>
2022-07-17 07:46:20 +0530

    sm7125-common: Remove virtual framebuffer  * It's largely for testing purposes and it consumes    a large amount of memory. If your hardware doesn't    have graphics then it can be useful, which isn't the    case for shipped devices.
    Change-Id: I2348718ad3afba846498572161fa7e83dd8b7ce9
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,7 +129,6 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
-BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
 # Media
 TARGET_USES_ION := true
