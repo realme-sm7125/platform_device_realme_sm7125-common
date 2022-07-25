7818fb3fe52e0b7d19724941aa42ce4bbec79bf7
Jose Adolfo Galdamez <josegalre@gmail.com>
2022-07-25 11:00:59 +0530

    sm7125-common: rootdir: Disable Gms Intent Operation Service
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -5822,3 +5822,6 @@ esac
 misc_link=$(ls -l /dev/block/bootdevice/by-name/misc)
 real_path=${misc_link##*>}
 setprop persist.vendor.mmi.misc_dev_path $real_path
+
+# Disable Gms Intent Operation Service
+pm disable com.google.android.gms/.chimera.GmsIntentOperationService
