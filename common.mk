d345dd4db293d2d6c91152398d30c6d0e6f3a676
Mayur <ultramayur123@gmail.com>
2022-08-25 21:14:20 +0530

    fixup!: sm7125-common: bluetooth fixes
    [debdeep199x: build A2DP blob]
    [debdeep199x: Drop bt_audio_hal_disabled prop]
    [debdeep199x: !fix: Merge with "BTAudio AIDL"]
    [kubersharma001: Add Bluetooth power overlays to sysprops]
    [kubersharma001 Use the ?= operator so BT properties can be overridden]
    
    Co-authored-by: debdeep199x <debdeep199x@gmail.com>
    Co-authored-by: kubersharma001 <kubersharma001@gmail.com>
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- common.mk
+++ common.mk
@@ -71,7 +71,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio-impl \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
@@ -466,6 +465,7 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1.vendor \
     android.hardware.biometrics.fingerprint@2.2.vendor \
     android.hardware.biometrics.fingerprint@2.3.vendor \
+    android.hardware.bluetooth.a2dp@1.0 \
     android.hardware.bluetooth.a2dp@1.0.vendor \
     android.hardware.bluetooth.audio@2.0.vendor \
     android.hardware.bluetooth@1.0.vendor \
