8205becfb7573ca780d3d3ccee36ed8d277571b3
Mayur <ultramayur123@gmail.com>
2022-09-03 15:08:19 +0530

    sm7125-common: RealmeParts: implement gaming mode
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- RealmeParts/src/org/aospextended/device/misc/MiscSettingsFragment.java
+++ RealmeParts/src/org/aospextended/device/misc/MiscSettingsFragment.java
@@ -32,6 +32,9 @@ public class MiscSettingsFragment extends PreferenceFragment implements
     private SwitchPreference mOTG;
     private static final String PREF_OTG = "otg";
     private static final String OTG_PATH = "/sys/class/power_supply/usb/otg_switch";
+    private SwitchPreference mGameSwitchPreference;
+    private static final String GAMESWITCH_ENABLE_KEY = "game_switch";
+    private static final String GAMESWITCH_NODE = "/proc/touchpanel/game_switch_enable";
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
@@ -44,12 +47,23 @@ public class MiscSettingsFragment extends PreferenceFragment implements
             mOTG.setSummary(R.string.otg_enable_summary_not_supported);
             mOTG.setEnabled(false);
         }
+        mGameSwitchPreference = (SwitchPreference) findPreference(GAMESWITCH_ENABLE_KEY);
+        if (FileUtils.fileExists(GAMESWITCH_NODE)) {
+            mGameSwitchPreference.setEnabled(true);
+            mGameSwitchPreference.setOnPreferenceChangeListener(this);
+        } else {
+            mGameSwitchPreference.setSummary(R.string.gameswitch_enable_summary_not_supported);
+            mGameSwitchPreference.setEnabled(false);
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         if (PREF_OTG.equals(preference.getKey())) {
             FileUtils.writeLine(OTG_PATH, (Boolean) newValue ? "1":"0");
+        }
+        if (GAMESWITCH_ENABLE_KEY.equals(preference.getKey())) {
+            FileUtils.writeLine(GAMESWITCH_NODE, (Boolean) newValue ? "1" : "0");
         }
         return true;
     }
