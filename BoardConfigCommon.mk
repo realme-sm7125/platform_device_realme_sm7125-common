9665e220a0af0113c1faee4528ee41939abd4a36
Mayur <ultramayur123@gmail.com>
2022-09-05 02:23:15 +0530

    Revert "sm7125-common: BC: clang"
    This reverts commit 19b4691b9db85b2faedbe9e3112aa692afc3a63c.
    
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,8 +109,8 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 KERNEL_LD := LD=ld.lld
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := clang-r437112
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r437112
+TARGET_KERNEL_CLANG_VERSION := proton
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton
 TARGET_KERNEL_SOURCE := kernel/realme/sm7125
 TARGET_KERNEL_CONFIG := atoll_defconfig
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
