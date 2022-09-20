3c8f99e150b76f6c0875eef2bc13147e85715969
Edwin Moquete <edwinmmoquete@gmail.com>
2022-09-20 20:55:40 +0530

    sm7125-common: Switch to legacy sepolicy_vndr
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -218,7 +218,7 @@ VENDOR_SECURITY_PATCH := 2020-10-01
 
 # Sepolicy
 SELINUX_IGNORE_NEVERALLOWS := true
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 #BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 
