4502bcf25be9cb7907a591ea10ac573491f5802a
Mayur <ultramayur123@gmail.com>
2023-01-25 18:44:41 +0000

    sm7125-common: build GCamGoPrebuilt-v3  also add it to aux allowlist
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- common.mk
+++ common.mk
@@ -144,6 +144,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
 
+# GCamGoPrebuilt
+PRODUCT_PACKAGES += \
+    GCamGOPrebuilt-V3
+
 # GPS
 PRODUCT_PACKAGES += \
     libsensorndkbridge
