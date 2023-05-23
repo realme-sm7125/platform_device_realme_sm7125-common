58d582b987a0b0c6b7ce4501616327c3816917e8
Mayur <ultramayur123@gmail.com>
2023-05-23 01:42:06 +0000

    Revert " sm7125-common: Remove virtual framebuffer"
    This reverts commit 3a76785303745b6175f946b1839b8d289d97242c.
    
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -134,6 +134,7 @@ BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
+BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
 # Media
 TARGET_USES_ION := true
