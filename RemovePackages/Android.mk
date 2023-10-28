22e698f7228298f48947b89bbf2fe636289494a4
Mayur <ultramayur123@gmail.com>
2023-10-28 05:12:54 +0000

    sm7125-common: removepacakges: add youtube music
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,7 +4,11 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := YouTube YTMusic SoundAmplifierPrebuilt
+LOCAL_OVERRIDES_PACKAGES := \
+    YouTube \
+    YTMusic \
+    YouTubeMusicPrebuilt \
+    SoundAmplifierPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
