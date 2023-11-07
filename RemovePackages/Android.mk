50ec89ca7ec56617fbfba5c365c01478123261bf
Mayur <ultramayur123@gmail.com>
2023-11-07 15:10:58 +0530

    sm7125-common: remove_package: remove more packages
    This reverts commit 26ddf2029429e4568b978d73ca1a3058f33e228e.
    
    Signed-off-by: Mayur <ultramayur123@gmail.com>

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -7,8 +7,40 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
     YouTube \
     YTMusic \
+    SoundAmplifierPrebuilt \
+    Camera2 \
+    ConnMO \
+    ConnMetrics \
+    DCMO \
+    DevicePolicyPrebuilt \
+    DMService \
+    Drive \
+    GCS \
+    GoogleCamera \
+    Maps \
+    Music \
+    MyVerizonServices \
+    OBDM_Permissions \
+    obdm_stub \
+    OdadPrebuilt \
+    OemDmTrigger \
+    OPScreenRecord \
+    Ornament \
+    PlayAutoInstallConfig \
+    SafetyHubPrebuilt \
+    SCONE \
+    ScribePrebuilt \
+    Showcase \
+    Snap \
+    Snap2 \
+    SnapdragonCamera \
+    SprintDM \
+    SprintHM \
+    Tycho \
+    USCCDM \
+    Videos \
     YouTubeMusicPrebuilt \
-    SoundAmplifierPrebuilt
+    PixelLiveWallpaperPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
